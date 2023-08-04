0f47a5ce731cdb1b2b94ded4e50eb7ddd164fd0e
sweyome <sweyome1@gmail.com>
2023-08-04 16:32:00 -0400

    update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+        self.value = Values[rank]
 
     def __str__(self):
         return self.rank + " of " + self.suit
